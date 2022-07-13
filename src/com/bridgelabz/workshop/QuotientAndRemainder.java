abf6f6ebef3df2642bfe7fb209429c08553fa079
VighneshBilgi <vhbilgi@gmail.com>
2022-07-13 21:37:11 +0530

    Added: Workshop-1

---
+++ src/com/bridgelabz/workshop/QuotientAndRemainder.java
@@ -0,0 +1,27 @@
+package com.bridgelabz.workshop;
+
+import java.util.Scanner;
+
+public class QuotientAndRemainder {
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Computing Quotient and Remainder:");
+        System.out.println();
+        System.out.println("Dividing number A by number B:");
+
+        System.out.println("Enter A:");
+        double a = sc.nextDouble();
+
+        System.out.println("Enter B:");
+        Double b = sc.nextDouble();
+
+        double quotient = (a/b);
+        double remainder = (a%b);
+
+        System.out.println(a+" (A) when divided by "+b+" (B) gives the quotient "+quotient+" and the remainder "+remainder);
+
+    }
+}
